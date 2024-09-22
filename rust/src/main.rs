f4171c12b19633047ce1cbf2edf04271ab60b1ee
sven <42868792+FreerGit@users.noreply.github.com>
2024-09-22 22:38:29 +0200

    gg

--- rust/src/main.rs
+++ rust/src/main.rs
@@ -2,19 +2,25 @@ use std::io::{self, Read};
 
 fn main() {
     let mut input = String::new();
-    io::stdin().read_to_string(&mut input).unwrap();
+    io::stdin().read_line(&mut input).unwrap();
 
-    let parts: Vec<i32> = input
-        .trim()
-        .split_whitespace()
-        .map(|x| x.parse().unwrap())
-        .collect();
-    let x: &i32 = parts.get(0).unwrap();
-    // let n: &i32 = parts.get(1).unwrap();
-    let mut acc = 0;
-    for i in parts[2..].iter() {
-        acc += x - i;
+    let mut input2 = String::new();
+    io::stdin().read_to_string(&mut input2).unwrap();
+
+    let lines: Vec<&str> = input2.trim().split_whitespace().collect();
+
+    let mut output: Vec<(usize, usize)> = Vec::new();
+    for (r, line) in lines.iter().enumerate() {
+        for (c, mine) in line.chars().enumerate() {
+            match mine {
+                _ if mine == '*' => output.push((r + 1, c + 1)),
+                _ => {}
+            }
+        }
     }
-    acc += x;
-    println!("{acc}");
+
+    println!("{}", output.len());
+    for o in output {
+        println!("{} {}", o.0, o.1);
+    }
 }
