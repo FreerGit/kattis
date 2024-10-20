8b015695c1958d6a688ab5196618c2d9abc980c3
sven <42868792+FreerGit@users.noreply.github.com>
2024-10-20 22:31:01 +0200

    gg

--- ocaml/src/main.ml
+++ ocaml/src/main.ml
@@ -16,19 +16,9 @@ open! Stdlib
    end) *)
 
 let () =
-  let n = read_line () |> int_of_string in
-  for _ = 1 to n do
-    let tot = ref 0 in
-    let k =
-      let line = read_line () in
-      Array.of_list (List.map int_of_string (String.split_on_char ' ' line))
-    in
-    for i = 1 to k.(0) do
-      tot := !tot + k.(i) - 1
-    done;
-    tot := !tot + 1;
-    Printf.printf "%d\n" !tot
-  done
+  let a, b = Scanf.scanf "%d %d\n" (fun a b -> a, b) in
+  let rec gcd a b = if b = 0 then a else gcd b (a mod b) in
+  Printf.printf "%d\n" (gcd a b)
 ;;
 
 (* Printf.printf "%d %d \n" !a !b *)
