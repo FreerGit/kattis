f1e56a6bcfba7bb1d83258288ed618bc2f24ca40
freergit <fredrik-98@live.se>
2025-02-13 16:21:39 +0100

    gg

--- py/main.py
+++ py/main.py
@@ -3,32 +3,19 @@ from collections import defaultdict
 
 # a,b = map(int, input().split())
 
-# Read input
-n = int(input()) 
-answers = input()
+label = int(input())
+cases = int(input())
 
-adrian_pattern = "ABC"  
-bruno_pattern = "BABC"  
-goran_pattern = "CCAABB"  
+total_time = 0
+has_box = label
+for _ in range(cases):
+    
+    t, answer = input().split()
+    t = int(t)
+    total_time += t
 
-def count_correct(pattern, answers):
-    pattern_length = len(pattern)
-    count = 0 
-    for i in range(len(answers)):
-        if answers[i] == pattern[i % pattern_length]:
-            count += 1
-    return count
+    if answer == "T" and total_time < 210:
+        has_box = (has_box % 8) + 1
 
-scores = {
-    "Adrian": count_correct(adrian_pattern, answers),
-    "Bruno": count_correct(bruno_pattern, answers),
-    "Goran": count_correct(goran_pattern, answers)
-}
 
-max_score = max(scores.values())
-
-winners = [name for name, score in scores.items() if score == max_score]
-
-print(max_score)
-for winner in sorted(winners): 
-    print(winner)
+print(has_box)
