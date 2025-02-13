d1d8b777f0c3374b3e22bc8dbba7863206648db8
freergit <fredrik-98@live.se>
2025-02-13 16:22:15 +0100

    gg

---
+++ py/ptice.py
@@ -0,0 +1,28 @@
+n = int(input()) 
+answers = input()
+
+adrian_pattern = "ABC"  
+bruno_pattern = "BABC"  
+goran_pattern = "CCAABB"  
+
+def count_correct(pattern, answers):
+    pattern_length = len(pattern)
+    count = 0 
+    for i in range(len(answers)):
+        if answers[i] == pattern[i % pattern_length]:
+            count += 1
+    return count
+
+scores = {
+    "Adrian": count_correct(adrian_pattern, answers),
+    "Bruno": count_correct(bruno_pattern, answers),
+    "Goran": count_correct(goran_pattern, answers)
+}
+
+max_score = max(scores.values())
+
+winners = [name for name, score in scores.items() if score == max_score]
+
+print(max_score)
+for winner in sorted(winners): 
+    print(winner)
---
+++ py/volim.py
@@ -0,0 +1,15 @@
+label = int(input())
+cases = int(input())
+
+total_time = 0
+has_box = label
+for _ in range(cases):
+    
+    t, answer = input().split()
+    t = int(t)
+    total_time += t
+
+    if answer == "T" and total_time < 210:
+        has_box = (has_box % 8) + 1
+
+print(has_box)
