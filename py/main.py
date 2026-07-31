22ea46d63a2c6bba6ae507dd70c0249e849ddcd1
sven <42868792+FreerGit@users.noreply.github.com>
2026-07-31 22:22:01 +0200

    gg

--- py/main.py
+++ py/main.py
@@ -1,33 +1,7 @@
+import sys
 import math
-from collections import defaultdict
-from collections import deque
-from collections import Counter
-from itertools import permutations
+import collections
 
-n = int(input())
-
-def get_digits(n):
-    digits = []
-    current = n
-    left = current
-    while n:
-        digits.append(n % 10)
-        n //= 10
-        
-    return digits
-
-nums = get_digits(n)
-possible = list(permutations(nums))
-
-m = n + 1
-larger = []
-for p in possible:
-    perm = int(''.join(list(map(str, p))))
-    if perm > n:
-        larger.append(perm)
-
-
-if len(larger) > 0:
-    print(min(larger))
-else:
-    print(0)
+decimal = int(input(), 2)
+octal = oct(decimal)
+print(octal[2:])
